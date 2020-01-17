da65e77644ce783d8f5f6cfaa37c74c625454e68
Neoterux <labfernandez2014@gmail.com>
2020-01-17 16:07:06 -0500

    Fixed jpg convertion

--- imageConversor.py
+++ imageConversor.py
@@ -2,11 +2,16 @@ from PIL import Image
 import os
 import re
 
+clear = lambda: os.system('cls' if os.name == 'nt' else 'clear')
+
 def make_dir(name):
     try:
         os.stat(name)
     except:
+        print("Creando directorio " + name + "...")
         os.mkdir(name)
+        print("Exitoso")
+        
 
 def find_file(pathList, extension):
     all_files = list(os.listdir("input"))
@@ -15,38 +20,56 @@ def find_file(pathList, extension):
     return pathList
 
 def get_option():
-    op = input("a)webm\nb)png\nc)jpg\n")
+    op = str(input("a)webp\nb)png\nc)jpg\nopcion: "))
     if op.lower() == "a":
         return ".webp"
     elif op.lower() == "b":
         return ".png"
-    elif op.lower == "c":
+    elif op.lower() == "c":
         return ".jpg"
     else:
-        return get_Option()
+        return get_option()
+
+def convertion(files, old_ext=".png", new_ext=".jpeg"):
+    
+        
+    for file in files:
+        image = Image.open("input\\" + file)
+        if(new_ext == ".jpg"):
+            background = Image.new("RGB", image.size, (255,255,255))
+            background.paste(image, mask=image.split()[-1])
+            
+            background.save("output\\" + file.lower().replace(old_ext, new_ext), 'JPEG', quality=85)
+        else:
+            image.save("output\\" + file.lower().replace(old_ext, new_ext))
+
+    
 
 make_dir("input")
-print("Seleccione la extension objetivo:")
-old_ext = get_option()
+input("Ingrese las imagenes a la carpeta input y presione enter para continuar")
+clear()
 
+print("\nSeleccione la extension objetivo:")
+old_ext = get_option()
+clear()
 print("Lista de archivos")
 
 files = []
 files = find_file(files, old_ext)
-print(files)
+print('\n'.join(map(str, files)))
 
 if not files:
-    print("No se han encontrado archivos con esa extension")
+    input("No se han encontrado archivos con esa extension")
     exit()
-    
-print("selecciones la extension a convertir")
+clear()
+
+
+print("selecciones la extension a convertir current extension [" + old_ext + "]")
 new_ext = get_option()
 
 if new_ext != old_ext:
     make_dir("output")
-    for image_file in files:
-        image = Image.open("input\\" + image_file)
-        image.save("output\\" + image_file.lower().replace(old_ext, new_ext))
+    convertion(files, old_ext, new_ext)
 
 print("Finalizado")
     
